e7087de17a72c0469ed9b279f69b2c0a1584dbed
srijan-paul <srijannr1@gmail.com>
2024-11-02 17:54:12 +0530

    test: add for loop test cases

---
+++ parser-tests/loops/for-statement.js
@@ -0,0 +1,9 @@
+for (let x = 1; x < 10; x++) { }
+for (; ;);
+for (var x = 0; ;);
+for (; x++;);
+for (; x; x += 1);
+for (; ; x += 1);
+for (; let = 1;);
+for (1; ;);
+for (var x=1,y=2;x < 0, y < 2 && y > 2; x++, y++);
